f46dc8d67117ff0c77deeb52fdb1e71c55f9156d
Kyle Bashour <kylebshr@me.com>
2016-02-16 00:06:54 -0500

    Update .gitignore

--- Pods/Target Support Files/Pods-Quotify/Pods-Quotify-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Quotify : NSObject
-@end
-@implementation PodsDummy_Pods_Quotify
-@end
--- Pods/Target Support Files/Pods-Quotify/Pods-Quotify-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_QuotifyVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_QuotifyVersionString[];
-
