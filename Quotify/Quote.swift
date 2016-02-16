ce9707441459ac3d94121aef0ea0da6da3764d57
Caleb <calebmennen@icloud.com>
2016-02-15 23:03:48 -0500

    more things

--- Quotify/Quote.swift
+++ Quotify/Quote.swift
@@ -7,11 +7,21 @@
 //
 
 import Foundation
+import Mapper
 
-struct Quote {
+struct Quote: Mappable{
+    //MARK: Properties
     let quote: String
     let author: String
-    let generated: Bool
-    let date: String
-    let source: NSDate
+    let generated: Bool?
+    let date: NSDate?
+    let source: String?
+    
+    init(map: Mapper) throws {
+        quote = try map.from("quote")
+        author = try map.from("author")
+        generated = map.optionalFrom("generated")
+        date = map.optionalFrom("date")
+        source = map.optionalFrom("source")
+    }
 }
